ccfb073ab9b9c8fff875f3472f2488ed82054666
Mike Elston <mike.elston@gmail.com>
2025-10-08 18:01:21 +1300

    WIP

--- src/flightgear.pl
+++ src/flightgear.pl
@@ -53,43 +53,46 @@ user:test :-
     read_line_to_string(Out, Line),
     sub_string(Line, _, _, _, "Primer reset to 0"),
     !,
-    flightgear_http_connection(C),
-    set_prop('/sim/current-view/view-number', 1, C),   % "Chase" view. Takes a few seconds to load
+    flightgear_http_connection(HTTP_Conn),
+    set_prop('/sim/current-view/view-number', HTTP_Conn, 1),   % "Chase" view. Takes a few seconds to load
     align_heading_indicator,
-    set_prop('/controls/engines/engine/throttle', 1.0, C),
-    thread_create(steer_heading_on_ground(338), _, [detached(true)]),
-    thread_create(fly_heading(340), _, [detached(true)]),
-    thread_create(fly_pitch(4), _, [detached(true)]).
+    set_prop('/controls/engines/engine/throttle', HTTP_Conn, 1.0),
+    steer_heading_on_ground.
+
+%    thread_create(fly_heading(340), _, [detached(true)]),
+%    thread_create(fly_pitch(4), _, [detached(true)]).
 
 
-%! steer_heading_on_ground(+Heading:integer) is det.
+%! steer_heading_on_ground is det.
 
-steer_heading_on_ground(Heading) :-
-    flightgear_http_connection(C),
-    repeat,
-    sleep(0.2),
-    get_prop('/position/altitude-agl-ft', Altitude_AGL_Feet, C),
-    Altitude_AGL_Feet < 10,
-    get_prop('/instrumentation/heading-indicator/indicated-heading-deg', Indicated_Heading, C),
-    direction_difference(Indicated_Heading, Heading, Error_Deg),   % +ve means right of intended track
-    Normalised_Error is Error_Deg / 180.0,  % [-1, +1]
-    clamped(-5.0 * Normalised_Error, -1, +1, Rudder),
-    % writeln(steering_on_ground(ih(Indicated_Heading), rudder(Rudder))),
-    set_prop('/controls/flight/rudder', Rudder, C),
-    fail.
+steer_heading_on_ground :-
+    flightgear_http_connection(HTTP_Conn),
+    pid_controller(( get_prop('/position/altitude-agl-ft', HTTP_Conn, Altitude_AGL_Feet),              % Guard
+                     Altitude_AGL_Feet < 10),
+                   heading_on_ground,                                                                  % Setpoint
+                   get_prop('/instrumentation/heading-indicator/indicated-heading-deg', HTTP_Conn),    % State_Value
+                   set_prop('/controls/flight/rudder', HTTP_Conn),                                     % Control
+                   direction_difference,                                                               % Error calculation
+                   0.03,                                                                               % P
+                   0,                                                                                  % I
+                   0).                                                                                 % D
 
+
+%!  heading_on_ground(-Heading) is det.
+
+heading_on_ground(338).
 
 %!  fly_heading(+Heading:integer) is det.
 
 fly_heading(Heading) :-
-    flightgear_http_connection(C),
+    flightgear_http_connection(HTTP_Conn),
     repeat,
     sleep(0.2),
-    get_prop('/position/altitude-agl-ft', Altitude_AGL_Feet, C),
+    get_prop('/position/altitude-agl-ft', HTTP_Conn, Altitude_AGL_Feet),
     Altitude_AGL_Feet >= 10,
 
-    get_prop('/instrumentation/heading-indicator/indicated-heading-deg', Indicated_Heading_Deg, C),
-    get_prop('/instrumentation/attitude-indicator/indicated-roll-deg', Indicated_Roll_Deg, C),
+    get_prop('/instrumentation/heading-indicator/indicated-heading-deg', HTTP_Conn, Indicated_Heading_Deg),
+    get_prop('/instrumentation/attitude-indicator/indicated-roll-deg', HTTP_Conn, Indicated_Roll_Deg),
     direction_difference(Indicated_Heading_Deg, Heading, Heading_Error_Deg),    % +ve means right of intended track
     (   Heading_Error_Deg < -10
     ->  Required_Roll_Deg = 10
@@ -104,7 +107,7 @@ fly_heading(Heading) :-
     clamped(-10 * Roll_Error_Deg / 180.0, -0.5, +0.5, Aileron),
 
     % writeln(fly_heading(heading(Indicated_Heading_Deg), roll_error(Roll_Error_Deg), required_roll(Required_Roll_Deg), actual_roll(Indicated_Roll_Deg), aileron(Aileron))),
-    set_prop('/controls/flight/aileron', Aileron, C),
+    set_prop('/controls/flight/aileron', HTTP_Conn, Aileron),
     fail.
 
 
@@ -114,14 +117,14 @@ fly_heading(Heading) :-
 %   Adjust trim to fly a specified pitch angle
 %
 %   Positive elevator-trim puts nose down
-%
+
 fly_pitch(Required_Pitch_Deg) :-
-    flightgear_http_connection(C),
+    flightgear_http_connection(HTTP_Conn),
     Sample_Time = 0.2,
     repeat,
     sleep(Sample_Time),
-    get_prop('/instrumentation/airspeed-indicator/indicated-speed-kt', Indicated_Speed_KT, C),
-    get_prop('/instrumentation/attitude-indicator/indicated-pitch-deg', Indicated_Pitch_Deg, C),
+    get_prop('/instrumentation/airspeed-indicator/indicated-speed-kt', HTTP_Conn, Indicated_Speed_KT),
+    get_prop('/instrumentation/attitude-indicator/indicated-pitch-deg', HTTP_Conn, Indicated_Pitch_Deg),
     writeln(ias(Indicated_Speed_KT)),
     (   Indicated_Speed_KT < 50
     ->  Required_Pitch_Deg_1 = 4
@@ -130,7 +133,7 @@ fly_pitch(Required_Pitch_Deg) :-
     Pitch_Error is Indicated_Pitch_Deg - Required_Pitch_Deg_1,   % +ve if pitch too high
     clamped(0.3 * Pitch_Error * Sample_Time, -0.5, +0.5, New_Elevator_Trim),
     writeln(fly_pitch(pitch(Indicated_Pitch_Deg), pitch_error(Pitch_Error), elevator_trim(New_Elevator_Trim))),
-    set_prop('/controls/flight/elevator-trim', New_Elevator_Trim, C),
+    set_prop('/controls/flight/elevator-trim', HTTP_Conn, New_Elevator_Trim),
     fail.
 
 
@@ -141,19 +144,19 @@ fly_pitch(Required_Pitch_Deg) :-
 %   NOT TESTED
 
 fly_indicated_airspeed(Required_Speed_KT) :-
-    flightgear_http_connection(C),
-    set_prop('/controls/flight/elevator-trim', 0, C),
+    flightgear_http_connection(HTTP_Conn),
+    set_prop('/controls/flight/elevator-trim', HTTP_Conn, 0),
     Sample_Time = 0.2,
     repeat,
     sleep(Sample_Time),
-    get_prop('/instrumentation/airspeed-indicator/indicated-speed-kt', Indicated_Speed_KT, C),
+    get_prop('/instrumentation/airspeed-indicator/indicated-speed-kt', HTTP_Conn, Indicated_Speed_KT),
     writeln(ias(Indicated_Speed_KT)),
     Indicated_Speed_KT > 40,
     Airspeed_Error is Indicated_Speed_KT - Required_Speed_KT,   % +ve if speed too high
-    get_prop('/controls/flight/elevator-trim', Current_Elevator_Trim, C),
+    get_prop('/controls/flight/elevator-trim', HTTP_Conn, Current_Elevator_Trim),
     New_Elevator_Trim is Current_Elevator_Trim - 0.001 * Airspeed_Error * Sample_Time,
     writeln(fly_indicated_airspeed(ias(Indicated_Speed_KT), elevator_trim(New_Elevator_Trim))),
-    set_prop('/controls/flight/elevator-trim', New_Elevator_Trim, C),
+    set_prop('/controls/flight/elevator-trim', HTTP_Conn, New_Elevator_Trim),
     fail.
 
 
@@ -162,13 +165,76 @@ fly_indicated_airspeed(Required_Speed_KT) :-
 %   Align the DI with the magnetic compass
 
 align_heading_indicator :-
-    flightgear_http_connection(C),
-    get_prop('/instrumentation/magnetic-compass/indicated-heading-deg', Magnetic_Compass_Indicated_Heading, C),
-    get_prop('/instrumentation/heading-indicator/indicated-heading-deg', Unaligned_Indicated_Heading, C),
-    get_prop('/instrumentation/heading-indicator/align-deg', Initial_Align_Deg, C),
+    flightgear_http_connection(HTTP_Conn),
+    get_prop('/instrumentation/magnetic-compass/indicated-heading-deg', HTTP_Conn, Magnetic_Compass_Indicated_Heading),
+    get_prop('/instrumentation/heading-indicator/indicated-heading-deg', HTTP_Conn, Unaligned_Indicated_Heading),
+    get_prop('/instrumentation/heading-indicator/align-deg', HTTP_Conn, Initial_Align_Deg),
     direction_difference(Magnetic_Compass_Indicated_Heading, Unaligned_Indicated_Heading, Heading_Indicator_Alignment_Error),
     Align_Deg is integer(Initial_Align_Deg + Heading_Indicator_Alignment_Error) mod 360,
-    set_prop('/instrumentation/heading-indicator/align-deg', Align_Deg, C).
+    set_prop('/instrumentation/heading-indicator/align-deg', HTTP_Conn, Align_Deg).
+
+
+%!  pid_controller(:Guard, :Setpoint_Pred, :State_Value_Pred, :Control_Pred, :Error_Pred, +P, +I, +D) is det.
+%
+%   PID controller to reduce the mismatch between the input setpoint
+%   and the desired state value. This predicate runs forever on its own
+%   thread.
+%
+%   @arg Guard run PID loop iff Guard succeeds
+%   @arg Setpoint_Pred name of goal to get the desired set point.
+%   e.g if Setpoint_Pred was required_roll_angle,
+%   required_roll_angle(-X) will be called.
+%
+%   @arg State_Value_Pred name of goal to get the current state value e.g if
+%   State_Value was roll_angle, roll_angle(X) will be called.
+%
+%   @arg Control_Pred name of goal to apply the output of the PID
+%   controller e.g. if Control_Output was set_aileron, set_aileron(X)
+%   will be called.
+%
+%   @arg Error_Pred name of goal to calculate the error from the
+%   Setpoint and the State_Value e.g. if Error_Pred was
+%   direction_difference, direction_difference(Setpoint, State_Value,
+%   Error) would be called
+%
+%   @arg P float the proportional gain
+%   @arg I float the integration gain
+%   @arg D float the derivative gain
+
+:- meta_predicate
+    pid_controller(:, :, :, :, :, +, +, +).
+
+pid_controller(Guard, Setpoint_Pred, State_Value_Pred, Control_Pred, Error_Pred, P, I, D) :-
+    call(State_Value_Pred, State_Value),
+    sample_time(Sample_Time),
+    thread_create(pid_controller_1(Guard, Setpoint_Pred, State_Value_Pred, Control_Pred, Error_Pred, P, I, D, Sample_Time, 0, State_Value), _, [detached(true)]).
+
+
+%! pid_controller_1(:Setpoint_Pred, :State_Value_Pred, :Control_Pred, :Error_Pred, +P, +I, +D, +Sample_Time, +Error_Sum, +Previous_Error) is det.
+
+:- meta_predicate
+    pid_controller_1(:, :, :, :, :, +, +, +, +, +, +).
+
+pid_controller_1(Guard, Setpoint_Pred, State_Value_Pred, Control_Pred, Error_Pred, P, I, D, Sample_Time, Error_Sum, Previous_Error) :-
+    (   Guard
+    ->  call(Setpoint_Pred, Setpoint),
+        call(State_Value_Pred, State_Value),
+        call(Error_Pred, Setpoint, State_Value, Error),
+        Error_Sum_1 is Error_Sum + Error,
+        Control is P * Error + I * Error_Sum * Sample_Time + D * (Error - Previous_Error) / Sample_Time,
+        call(Control_Pred, Control)
+    ;   true
+    ),
+    sleep(Sample_Time),
+    pid_controller_1(Guard, Setpoint_Pred, State_Value_Pred, Control_Pred, Error_Pred, P, I, D, Sample_Time, Error_Sum_1, Error).
+
+
+%!  sample_time(-Sample_Time) is det.
+%
+%   @arg Sample_Time float sample time in seconds
+
+sample_time(0.2).
+
 
 
 %!  direction_difference(+H1, +H2, -Diff) is det.
@@ -197,37 +263,12 @@ flightgear_http_connection(HTTPConnection) :-
     py_call('flightgear_python.fg_if':'HTTPConnection'(localhost, 8080, timeout_s=10), HTTPConnection).
 
 
-%!  set_prop(+Property_Path, +Value, +HTTP_Connection) is det.
+%!  set_prop(+Property_Path, +HTTP_Conn, +Value) is det.
 
-set_prop(Property_Path, Value, HTTP_Connection) :-
-    py_call(HTTP_Connection:set_prop(Property_Path, Value)).
+set_prop(Property_Path, HTTP_Conn, Value) :-
+    py_call(HTTP_Conn:set_prop(Property_Path, Value)).
 
-%!  get_prop(+Property_Path, -Value, +HTTP_Connection) is det.
+%!  get_prop(+Property_Path, +HTTP_Conn, -Value) is det.
 
-get_prop(Property_Path, Value, HTTP_Connection) :-
-    py_call(HTTP_Connection:get_prop(Property_Path), Value).
-
-
-
-
-
-    /*
-     *
-     *
-    set_prop('/position/altitude-ft', 2000, C),
-
-    set_prop('/controls/engines/current-engine/carb-heat', 0, C),
-    set_prop('/controls/engines/engine/mixture', 0.6, C),
-    set_prop('/controls/engines/engine/throttle', 0.8, C),
-
-    set_prop('/controls/switches/starter', true, C),
-    sleep(5.0),
-    set_prop('/controls/switches/starter', false, C),
-
-
-    get_prop('/position/altitude-ft', A0, C),
-    between(0, 10, I),
-    A1 is A0 + I * 100,
-    set_prop('/position/altitude-ft', A1, C),
-    fail.
-*/
+get_prop(Property_Path, HTTP_Conn, Value) :-
+    py_call(HTTP_Conn:get_prop(Property_Path), Value).
