b35c616d3da8f4cb0b1a53ad73e505e0ec9ad34a
Mike Elston <mike.elston@gmail.com>
2025-11-02 17:01:18 +1300

    WIP

--- src/spi.pl
+++ src/spi.pl
@@ -43,78 +43,86 @@ depends on the location of the system's poles within the unit circle in the z-pl
 See also https://courses.cs.duke.edu/spring07/cps111/notes/03.pdf
 */
 
+:- meta_predicate
+   sodt(2, -, +, +, +, +, +, +, +),
+   sodt_1(+, 2, +, +, +, +, +, +, +, +, +, +, +).
+
+
 %!  ts is semidet.
 
 user:ts :-
-   N = 500,
-   step_series(N, 0.1, [p(_, X2), p(_, X1)|X]),
-   sodt(X, Y, X2, X1, 0, 0, 0, 1, 0.1, 1.1, 1.2, 1.0),
+   N = 100,
+   sodt(step(0.01, 0.05), Y, 0, N, 0.1, 0.99, 0.04, 1, 0),
    new(W, auto_sized_picture(sodt)),
    send(W, max_size, size(2000, 600)),
    send(W, display, new(Plotter, plotter)),
    send(Plotter, axis, plot_axis(x, 0.0, N, @(default), 1500)),
-   send(Plotter, axis, plot_axis(y, 0.0, 2.0, @(default), 400)),
+   send(Plotter, axis, plot_axis(y, 0.0, 0.05, @(default), 400)),
    send(Plotter, graph, new(Plot, plot_graph)),
    send(Plot, colour, green),
    send(W, open),
    member(p(T, V), Y),
-   send(Plot, append, T, V),
+   writeln((T,V)),
+   T_Plot is T * 500,
+   send(Plot, append, T_Plot, V),
    sleep(0.01),
    fail.
 
+%! step(+Time_Of_Step, +Step_Size, +Time, -X) is det.
 
-%!  sodt(+X, -Y, +X2, +X1, +Y2, +Y1, +I, +A1, +A2, +B0, +B1, +B2)
-%!       is det.
-%
-%  X1 means x[k-1], X2 means x[k-1] etc, same for Y1 etc
+step(Time_Of_Step, Step_Size, T, X) :-
+   (   T =< Time_Of_Step
+   ->  X = 0
+   ;   X = Step_Size
+   ).
+
+
+%!  sodt(+X_Pred, -Y_Series, +Time, +N, +Time_Step, +A1:number,
+%!       +A2:number, +B1:number, +B2:number) is det.
 %
 %  Second-order discrete-time linear system
 %  Difference Equation: A second-order discrete-time linear system can be represented by the
 %  following general form of a difference equation:
 %
-%  y[k]+a1.y[k-1]+a2.y[k-2] = b0.x[k]+b1.x[k-1]+b2.x[k-2]
-%
-%  H(Z) = B0 + B1 * Z ^ -1 + B2 * Z ^ -2 / 1 + A1 * Z ^ -1 + A2 * Z ^ -2
-%
-%  @arg X list of p(T, V)
-%  @arg Y list of p(T, V)
+%  y[k]+a1.y[k-1]+a2.y[k-2] = b1.x[k-1]+b2.x[k-2]
 
-sodt([], [], _, _, _, _, _, _, _, _, _, _).
-sodt([p(T0, X0)|L1], [p(T0, Y0)|L2], X2, X1, Y2, Y1, I, A1, A2, B0, B1, B2) :-
-    Y0 is B0 * X0 + B1 * X1 + B2 * X2 - A1 * Y1 - A2 * Y2,
-    II is I + 1,
-    sodt(L1, L2, X1, X0, Y1, Y0, II, A1, A2, B0, B1, B2).
+%  @arg X_Pred closure foo(..., Time, X)
+%  @arg Y_Series list of p(T, V)
+
+sodt(X_Pred, Y_Series, Time, N, Time_Step, A1, A2, B1, B2) :-
+   sodt_1(N, X_Pred, Y_Series, Time, Time_Step, 0, 0, 0, 0, A1, A2, B1, B2).
 
 
-%! step_series(+Number_Of_Steps, +Time_Step, -Step_Series: list) is det.
-%
-%   @arg Step_Series list of p(Time, Value)
+%!  sodt_1(+K, +X_Pred, -Y_Series, +Time, +Time_Step, +X2, +X1, +Y2,
+%!         +Y1, +A1, +A2, +B1, +B2) is det.
+%  X1 means x[k-1], X2 means x[k-1] etc, same for Y1 etc
 
-step_series(N, Time_Step, [p(0.0, 0.0), p(Time_Step, 0.0)|T]) :-
-   N1 is N-2,
-   Time is 2 * Time_Step,
-   step_series_1(N1, Time_Step, Time, T).
-
-step_series_1(0, _, _, []) :-
+sodt_1(0, _, [], _, _, _, _, _, _, _, _, _, _) :-
    !.
-step_series_1(N, Time_Step, Time, [p(Time, 1.0)|T]) :-
-   NN is N-1,
-   Time_1 is Time + Time_Step,
-   step_series_1(NN, Time_Step, Time_1, T).
+sodt_1(K, X_Pred, [p(T, Y0)|Y_Series], T, Time_Step, X2, X1, Y2, Y1, A1, A2, B1, B2) :-
+    call(X_Pred, T, X0),
+    Y0 is B1 * X1 + B2 * X2 - A1 * Y1 - A2 * Y2,
+    KK is K-1,
+    T_Next is T + Time_Step,
+    sodt_1(KK, X_Pred, Y_Series, T_Next, Time_Step, X1, X0, Y1, Y0, A1, A2, B1, B2).
+
 
 
 %  fit is det.
 
 :- dynamic p/2.
-
+/*
 user:fit :-
-    consult(dat('step_response.dat')),
-    findall(p(T,V), p(T,V), L1),
-    L1 = [p(T0, _)|_],
-    rebase_time(L1, T0, L2),
-    writeln(L2).
-
-
+   N = 5,
+   consult(dat('step_response.dat')),
+   once(findnsols(N, p(T,V), p(T,V), L)),
+   L = [p(T0, _)|_],
+   rebase_time(L, T0, Measured_Step_Response),
+   step_series
+   sodt(X, Y, A1, A2, B0, B1, B2),
+   writeln(X),
+   writeln(Y).
+*/
 %!  rebase_time(+L1, +T0, -L2) is det.
 
 rebase_time([], _, []).
